8286ff2b83af587d79996230d93d8a1d0fbe71b3
TianXing Xue <76930817+veritas0518@users.noreply.github.com>
2021-06-21 13:21:07 +0800

    combines the point class into the triangle class.
    【问题描述】先定义Point类，再定义Triangle类，在Triangle类中定义三个Point对象来表示一个三角形的三个顶点，然后定义一个方法isTri()对这三个点是否能构成三角形进行判定。构造方法先调用isTri()，如果三个点能够构成三角形，则对三角形对象进行初始化，否则给出提示并退出程序。最后定义两个方法getPerimeter()和getArea()，分别求三角形的周长和面积。
    在TestTriangle类的main()方法中创建一个Triangle对象，从键盘输入该对象三个顶点的坐标，然后求该三角形的周长和面积（小数点后保留3位），或者给出不能构成三角形的提示。
    
    【样例输入1】
    1 1
    2 2
    3 3
    
    【样例输出1】
    Please enter the coordinates of p1:
    1 1
    Please enter the coordinates of p2:
    2 2
    Please enter the coordinates of p3:
    3 3
    Can't form a triangle!
    
    【样例输入2】
    1 1
    2 0
    0 3
    
    【样例输出2】
    Please enter the coordinates of p1:
    1 1
    Please enter the coordinates of p2:
    2 0
    Please enter the coordinates of p3:
    0 3
    Perimeter=7.256
    Area=0.500

---
+++ TestTriangle.java
@@ -0,0 +1,47 @@
+import java.util.Scanner;
+
+public class TestTriangle {
+    public static void main(String[] args) {
+
+        Triangle t1=new Triangle();
+    }
+}
+class Point{
+    protected double x;
+    protected double y;
+
+}
+class Triangle extends Point{
+    Point p1=new Point();
+    Point p2=new Point();
+    Point p3=new Point();
+    Scanner s = new Scanner(System.in);
+    Triangle(){
+    p1.x = s.nextDouble();
+    p1.y = s.nextDouble();
+     p2.x = s.nextDouble();
+     p2.y = s.nextDouble();
+    p3.x = s.nextDouble();
+    p3.y = s.nextDouble();
+        this.isTri();
+}
+
+    public void isTri(){
+        System.out.println("* p1*\n* p2*\n* p3*\n");
+    double a = Math.sqrt( (p1.x-p2.x)*(p1.x-p2.x)+ (p1.y-p2.y)*(p1.y-p2.y) );
+    double b = Math.sqrt( (p3.x-p2.x)*(p3.x-p2.x)+ (p3.y-p2.y)*(p3.y-p2.y) );
+    double c = Math.sqrt( (p1.x-p3.x)*(p1.x-p3.x)+ (p1.y-p3.y)*(p1.y-p3.y) );
+        if(a+b>c && a+c>b && b+c>a && a-b<c && a-c<b && b-c<a){
+            double Perimeter = a + b + c;
+            double p = (a+b+c)/(2.0);
+            double Area = Math.sqrt(Math.abs(p*(p-a)*(p-b)*(p-c)));
+            System.out.printf("Perimeter=%f*Area=%f*",Perimeter,Area);
+        }
+        else{
+            System.out.println("Can't form a triangle!");
+        }
+
+    }
+}
+
+
